a4cf23cc460d8a8c65d86057aeec3c4ac1530750
Atul Balaji <atulapra@gmail.com>
2018-05-26 22:03:33 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Cats Vs Dogs in Keras
-Keras implementation of classification between dogs and cats.
+
+* Kaggle's Cats Vs Dogs challenge is a great way for deep learning learners to quickly implement CNNs, transfer learning, fine-tuning,etc and experiment with their knowledge.
+* This repository is a collection of the gists referenced in [this](https://blog.keras.io/building-powerful-image-classification-models-using-very-little-data.html) keras blog post, along with some corrections to remove common errors and to ensure compatibility in both Python 2 and 3. 
+
+## Setup
+
+* Download the dataset from [here](https://anonfile.com/y0h9D3edbd/data.tar.gz) and unzip it in the current folder.
+* Download the vgg16 weights from [here.](https://anonfile.com/Gbh8D1efba/vgg16_weights.h5)
+* I have trained these models on Nvidia GeForce GTX 1050 Ti GPU, and hence my training time would be quite low as compared to CPU. So, if you are going to train this in CPU, it might take a susbstantial amount of time.
+
+## Usage
+
+* The program `conv.py` employs a 3 layer convolutional neural network for classification. Just run `python conv.py`. The weights for this model will be saved as `first_try.h5`. Training takes ~15s per epoch. We get a test accuracy of ~80%.
+* The program `bottleneck.py` employs a vgg16 network with FC layers retrained. Run `python bottleneck.py`. The weights will be saved as `bottleneck_fc_model.h5`. Training takes only 1s per epoch. We get a test accuracy of ~90%.
+* The program `finetune.py` is used to fine tune the model trained above. Run `python finetune.py`. Training takes ~85s per epoch.
+
+* Note that `finetune.py` can be run only after you have saved the weights `bottleneck_fc_model.h5`, which is obtained by running `bottleneck.py`.
+
+
+
---
+++ conv.py
@@ -0,0 +1,94 @@
+"""
+Here, we will be using a small ConvNet with just 3 conv layers to classify dogs vs cats.
+Loss: binary crossentropy (softmax)
+Optimizer: rmsprop
+
+Step 1: Define the model
+Step 2: Configure the image dataset generators for train, test
+Step 3: Fit the model to determine accuracy.
+"""
+
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import backend as K
+
+img_width, img_height = 150, 150
+train_data_dir = 'data/train'
+validation_data_dir = 'data/validation'
+nb_train_samples = 2000
+nb_validation_samples = 800
+epochs = 50
+batch_size = 16
+
+# Our case is channels first
+if K.image_data_format() == 'channels_first':
+    input_shape = (3, img_width, img_height)
+else:
+    input_shape = (img_width, img_height, 3)
+
+## Define the model
+# We used sigmoid since it is best for binary classification
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+print('Model Summary \n')
+model.summary()
+
+# model.load_weights('./first_try.h5')
+model.compile(loss='binary_crossentropy',optimizer='rmsprop',metrics=['accuracy'])
+print('\nWeights Loaded \n')
+
+# this is the augmentation configuration we will use for training
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+# this is the augmentation configuration we will use for testing:
+# only rescaling
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+## Defining the data generators.
+# These will generate batches of data from our images.
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary')
+
+validation_generator = test_datagen.flow_from_directory(
+    validation_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary')
+
+## Fit the model and determine training, test accuracy.
+print('\nFitting model\n')
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=nb_train_samples // batch_size,
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=nb_validation_samples // batch_size)
+
+model.save_weights('first_try.h5')
